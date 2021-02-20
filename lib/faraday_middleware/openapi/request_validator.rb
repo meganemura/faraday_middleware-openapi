7437c348c3d1852957191d0acf5820f729d9c164
meganemura <meganemura@users.noreply.github.com>
2021-02-20 12:03:28 +0900

    Refactor RequestValidator

--- lib/faraday_middleware/openapi/request_validator.rb
+++ lib/faraday_middleware/openapi/request_validator.rb
@@ -4,20 +4,7 @@ module FaradayMiddleware
   module OpenAPI
     class RequestValidator < Faraday::Middleware
       def on_request(env)
-        content_type = env.request_headers['Content-Type']
-        operation = request_operation(env.method, env.url.path)
-        operation.validate_request_parameter(env.params || {}, {})
-        request_body = case content_type
-                       when 'application/json' # Use regexp
-                         begin
-                           JSON.parse(env.request_body)
-                         rescue
-                           nil
-                         end
-                       end
-        operation.validate_request_body(content_type, request_body)
-      rescue OpenAPIParser::NotExistRequiredKey, OpenAPIParser::NotNullError => e
-        raise ::FaradayMiddleware::OpenAPI::Error.new(e.message)
+        validate(env)
       end
 
       def initialize(app, options = {})
@@ -30,12 +17,33 @@ module FaradayMiddleware
 
       attr_reader :schema_path
 
-      def openapi_parser
-        @openapi_parser ||= OpenAPIParser.parse(YAML.load_file(schema_path), coerce_value: false)
+      def validate(env)
+        content_type = env.request_headers['Content-Type']
+        operation = request_operation(env.method, env.url.path)
+        operation.validate_request_parameter(env.params || {}, {})
+        request_body = extract_request_body(content_type, env.request_body)
+        operation.validate_request_body(content_type, request_body)
+      rescue OpenAPIParser::NotExistRequiredKey, OpenAPIParser::NotNullError => e
+        raise ::FaradayMiddleware::OpenAPI::Error.new(e.message)
+      end
+
+      def extract_request_body(content_type, request_body)
+        case content_type
+        when 'application/json' # Use regexp
+          begin
+            JSON.parse(request_body)
+          rescue
+            nil
+          end
+        end
       end
 
       def request_operation(method, path)
         openapi_parser.request_operation(method, path)
+      end
+
+      def openapi_parser
+        @openapi_parser ||= OpenAPIParser.parse(YAML.load_file(schema_path), coerce_value: false)
       end
     end
   end
