9cd1d933f160f16b8a66efb43a9202b025ef5fe8
meganemura <meganemura@users.noreply.github.com>
2021-02-20 11:28:30 +0900

    Remove unnecessary spec

--- spec/faraday_middleware/openapi_spec.rb
+++ spec/faraday_middleware/openapi_spec.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe FaradayMiddleware::OpenAPI do
-  it "has a version number" do
-    expect(FaradayMiddleware::OpenAPI::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
